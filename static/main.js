868b6e777f75d70ed8b10d352d67d7d785c8a627
Nathan Hamal <nathanhamal1@gmail.com>
2013-02-19 21:49:26 -0500

    layout changes, canvas setup and scrolling only effects sidebar

--- static/main.js
+++ static/main.js
@@ -1,2 +1,29 @@
+var canvas = document.getElementById("blueprint");
+var ctx = canvas.getContext("2d");
+
+
 function main() {
+	setUpScreen();
+	setUpBlueprint();
 }
+
+function setUpScreen(){
+	$(document).ready(function(){
+		$('#drawtable').css('width'," "+ $(window).width()-300+"px");
+	})
+	$(window).resize(function(){
+		$('#drawtable').css('width'," "+ $(window).width()-300+"px");
+	})
+}
+
+
+function setUpBlueprint(){
+	ctx.fillStyle = "#001087"
+	ctx.fillRect(0,0,canvas.width,canvas.height);
+}
+
+
+
+
+
+main();
