96596f179da9ce96044080b6b9245dc09158dd11
Dylan Mikus <dbmikus@gmail.com>
2013-02-24 15:28:48 -0500

    Drawing functions take context and HMTL file loads in furnituremaker.js

--- static/furnituremaker.js
+++ static/furnituremaker.js
@@ -13,36 +13,37 @@ assembly : []
 
 */
 
-//orientation is stored as radians, color as a hex, location as an array [x,y], dimensions stored as an array [width, height]
-function drawRectangle( location , orientation, color, dimensions ) {
-
+// orientation is stored as radians, color as a hex, location as an array [x,y],
+// dimensions stored as an array [width, height]
+// ctx should be a 2d canvas context
+function drawRectangle(ctx, location, orientation, color, dimensions) {
 	ctx.fill = color;
-	
-	//Move context to the point you are rotating around (the center of your drawing) 
+
+	//Move context to the point you are rotating around (the center of your drawing)
 	ctx.translate(location[0], location[1]);
 	//Rotate the canvas(angle in radians)
 	ctx.rotate(orientation);
 	//draw the object
 	ctx.fillRect(dimensions[0]/2, dimensions[1]/2, dimensions[0], dimensions[1]);
 	//rotate the canvas back in the opposite direction
-	ctx.rotate(-(orientation)); 
+	ctx.rotate(-(orientation));
 }
 
-function drawCircle( location, color, dimensions) {
+function drawCircle(ctx, location, color, dimensions) {
 	ctx.arc(location[0], location[1], dimensions[1] , 0 , 2 * Math.PI, false)
 }
 
-function drawOval(location, orientation, color, dimensions){
-	ctx.fill = color; 
+function drawOval(ctx, location, orientation, color, dimensions){
+	ctx.fill = color;
 	//save the context
 	ctx.save();
 	//move the origin to where we are drawing
 	ctx.translate(location[0], location[1]);
 	//scale the dimensions so we can make an oval
 	ctx.scale( dimensions[0] * dimensions[1] / 4 , 1);
-	
+
 	//rotate the canvas so we get the desired orientation
-	ctx.rotate(orientation); 
+	ctx.rotate(orientation);
 	// draw the oval
 	ctx.beginPath();
 	ctx.arc(location[0], location[1], dimensions[1] , 0 , 2 * Math.PI, false);
@@ -53,5 +54,4 @@ function drawOval(location, orientation, color, dimensions){
 	//style things can go here
 	//
 	//
-	
 }
