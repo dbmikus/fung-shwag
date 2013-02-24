9aeee050f13d8700b2a56ec264caabd0c93d09b2
Dylan Mikus <dbmikus@gmail.com>
2013-02-24 16:51:56 -0500

    Fixed drawEllipse function

--- static/furnituremaker.js
+++ static/furnituremaker.js
@@ -45,60 +45,41 @@ function drawEllipse(ctx, location, orientation, color, dimensions) {
     var y = location[1];
     var w = dimensions[0];
     var h = dimensions[1];
-
     var kappa = .5522848;
-    ox = (w / 2) * kappa, // control point offset horizontal
-    oy = (h / 2) * kappa, // control point offset vertical
-    xe =  w,           // x-end
-    ye =  h,           // y-end
-    xm =  w / 2,       // x-middle
-    ym =  h / 2;       // y-middle
+
 	//save original coordinates
 	ctx.save();
-	
+
     ctx.fillStyle = color;
 	//Move context to the point you are rotating around (the center of your drawing)
 	ctx.translate(location[0], location[1]);
+
+    // Redefining variables so that we draw around origin
+    x = -(w/2);
+    y = -(h/2);
+    var ox = (w / 2) * kappa; // control point offset horizontal
+    var oy = (h / 2) * kappa; // control point offset vertical
+    var xe = x + w;           // x-end
+    var ye = y + h;           // y-end
+    var xm = x + w / 2;       // x-middle
+    var ym = y + h / 2;       // y-middle
+
 	//Rotate the canvas(angle in radians)
 	ctx.rotate(orientation);
-	
+
 	//draw the ellipse
     ctx.beginPath();
-    ctx.moveTo(0, ym);
-    ctx.bezierCurveTo(0, ym - oy, xm - ox, 0, xm, 0);
-    ctx.bezierCurveTo(xm + ox, 0, xe, ym - oy, xe, ym);
+    ctx.moveTo(x, ym);
+    ctx.bezierCurveTo(x, ym - oy, xm - ox, y, xm, y);
+    ctx.bezierCurveTo(xm + ox, y, xe, ym - oy, xe, ym);
     ctx.bezierCurveTo(xe, ym + oy, xm + ox, ye, xm, ye);
-    ctx.bezierCurveTo(xm - ox, ye, 0, ym + oy, 0, ym);
+    ctx.bezierCurveTo(xm - ox, ye, x, ym + oy, x, ym);
+
     ctx.closePath();
     ctx.fill();
-	
+
 	//rotate back
 	ctx.rotate(-(orientation));
 	//restore the canvas origin
-	ctx.restore();
-}
-
-
-function drawOval(ctx, location, orientation, color, dimensions){
-	//save the context
-	ctx.save();
-
-	//move the origin to where we are drawing
-	ctx.translate(location[0], location[1]);
-	//scale the dimensions so we can make an oval
-	ctx.scale(dimensions[0] / 4 , 1);
-	//rotate the canvas so we get the desired orientation
-	ctx.rotate(orientation);
-
-	ctx.fillStyle = color;
-	// draw the oval
-	ctx.beginPath();
-    // We draw with the x and y coordinates set to 0 since we moved our origin
-	ctx.arc(0, 0, dimensions[1], 0, 2 * Math.PI, false);
-	//rotate the canvas back in the opposite (might not be necesarry with ctx.restore();
-	ctx.rotate(-(orientation));
-    ctx.fill();
-
-	//restore the canvas to its unstretched state
 	ctx.restore();
 }
